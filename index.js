1b10be079ec331579e0a7dc5231d8246f19ca871
galaxycomic <panda12211331@gmail.com>
2023-11-01 00:05:16 +0100

    basic's added
    we hebben standaard disc bot template

---
+++ index.js
@@ -0,0 +1,10 @@
+require(`dotenv`).config();
+
+const { Client, Events, GatewayIntentBits } = require('discord.js');
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
+
+client.once( Events.ClientReady, c => {
+    console.log(`Logged in as ${c.user.tag}`);
+});
+
+client.login(process.env.TOKEN);
